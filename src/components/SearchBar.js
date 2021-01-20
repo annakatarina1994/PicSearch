7d53e9c484dcbca11e7e147202adc55ef5ea7266
Anna Lizon <annakatarina1994@gmail.com>
2021-01-20 16:12:12 -0500

    Making sure app still works and it does yay

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,16 +11,20 @@ class SearchBar extends React.Component{
 
     render(){
         return(
+          <div>
+          <h1 class="ui header">Pic Finder</h1>
           <div className="ui segment">
             <form onSubmit={this.onFormSubmit} className="ui form">
               <div className="field">
                 <label>Image Search</label>
                   <input 
-                    type="text" 
+                    type="text"
+                    placeholder="Search here..." 
                     value={this.state.term} 
                     onChange={(e)=> this.setState({term: e.target.value})} />
               </div>
             </form>
+          </div>
           </div>
         );
     }
